03384c207abd24eb0b5a994065295c09aaea776a
Anjara37 <159740020+Anjara37@users.noreply.github.com>
2024-09-08 11:33:11 +0400

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,13 @@
 let myHeading = document.querySelector('h1');
 myHeading.textContent = 'Bonjour, monde !';
-let myVariable = 'Veuillez vous connecter en appuyant sur le boutton en bas de la page😊';
+let myVariable = 'Bob';
 myHeading.addEventListener('click', function() {
 
     if (myHeading.textContent === 'Bonjour, monde !') {
     myHeading.textContent= myVariable;
     
 } else {
-    myHeading.textContent = 'Vous etes sur le chemin de l\'intelligence';
+    myHeading.textContent = 'Bonjour, monde!';
     
 }});
 
